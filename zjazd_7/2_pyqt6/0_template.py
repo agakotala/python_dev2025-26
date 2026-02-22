9190121f42297cf446af631567328a2cbf5a4150
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-02-22 09:49:17 +0100

    1

---
+++ zjazd_7/2_pyqt6/0_template.py
@@ -0,0 +1,12 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QDialog, QPushButton
+from PyQt6.QtCore import QSize
+import sys
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+
+
+
+
+window.show()
+app.exec()
---
+++ zjazd_7/2_pyqt6/1_first_app.py
@@ -0,0 +1,12 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
+import sys
+
+app = QApplication(sys.argv)
+# window = QDialog()
+# window = QWidget()
+window = QMainWindow()
+window.statusBar().showMessage('Status....')
+
+window.show()
+app.exec()
+
